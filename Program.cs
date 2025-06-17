5c2579ac79a5a828b47ed0d0e1faeffdfba6c394
juan-k <djjuankalexis18@gmail.com>
2025-06-17 13:41:38 -0300

    Terminando punto 2

--- Program.cs
+++ Program.cs
@@ -5,8 +5,20 @@ var tareas = await GetTareas();
 
 foreach(var tar in tareas)
 {
-    Console.WriteLine($"Tarea id: {tar.id} Titulo: {tar.title} UserID: {tar.userId}" );
+    if(tar.completed)
+        Console.WriteLine($"Titulo: {tar.title} Estado: {tar.completed}" );
 }
+
+Console.WriteLine("------------------------------------------------------");
+
+foreach(var tar in tareas)
+{
+    if(!tar.completed)
+        Console.WriteLine($"Titulo: {tar.title} Estado: {tar.completed}" );
+}
+
+string textoGuardar = JsonSerializer.Serialize(tareas);
+GuardarArchivoTexto("tareas.json" , textoGuardar);
 
 static async Task<List<Tarea>> GetTareas()
 {
@@ -27,6 +39,24 @@ static async Task<List<Tarea>> GetTareas()
         return null;
     }
 }
+void GuardarArchivoTexto(string nombreArchivo, string datos)
+    {
+        using (var archivo = new FileStream(nombreArchivo, FileMode.Create))
+        {
+            using (var strWriter = new StreamWriter(archivo))
+            {
+                strWriter.WriteLine("{0}", datos);
+                strWriter.Close();
+            }
+        }
+    }
+
+/* //Guardo el archivo
+Console.WriteLine("--Serializando--");
+string alumnosJson = JsonSerializer.Serialize(listadoAlumnos);
+Console.WriteLine("Archivo Serializado : " + alumnosJson);
+Console.WriteLine("--Guardando--");
+miHelperdeArchivos.GuardarArchivoTexto(NombreArchivo, alumnosJson) */
 
 
 
